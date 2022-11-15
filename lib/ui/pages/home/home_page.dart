d0ef6c58e3db3dbd24c719c497a0a0671c210444
Nguyệt Ánh <55714975+Sakeshioyaki@users.noreply.github.com>
2022-11-15 17:06:00 +0700

    update

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -3,7 +3,7 @@ import 'package:dog_app/common/app_text_styles.dart';
 import 'package:dog_app/ui/pages/detailBreed/detail_breed_page.dart';
 import 'package:dog_app/ui/pages/home/home_cubit.dart';
 import 'package:dog_app/ui/pages/home/home_state.dart';
-import 'package:dog_app/ui/pages/home/widget/breeds_list_dialog_page.dart';
+import 'package:dog_app/ui/pages/home/widgets/breeds_list_dialog_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
